42f5eed20500cee55e9f7d15fb2882a5dc6076e4
Ryan <ryancampos393@gmail.com>
2026-03-08 18:29:39 -0400

    att

---
+++ config.js
@@ -0,0 +1,6 @@
+// ATENÇÃO: Este arquivo NÃO deve ser enviado para o GitHub!
+// Adicione "config.js" no seu arquivo .gitignore
+window._gcConfig = {
+  clientId: '301377588394-3c1ltq6j4jtc8dia01rrr02aju1210b8.apps.googleusercontent.com',
+  apiKey:   'AIzaSyBWSJ_LIjoR-WZXFN5jvL61mKfmqRS00Wg'
+};
